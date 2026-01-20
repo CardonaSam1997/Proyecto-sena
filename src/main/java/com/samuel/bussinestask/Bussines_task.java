cec0ccf9c5b640789030e4951d9e03ce8a603975
Samuel Cardona <scardona0297@gmail.com>
2026-01-20 09:38:48 -0500

    Fase inicial

---
+++ src/main/java/com/samuel/bussinestask/Bussines_task.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.samuel.bussinestask;
+
+/**
+ *
+ * @author Admin
+ */
+public class Bussines_task {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
